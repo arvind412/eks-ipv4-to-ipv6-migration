93b57cd9dac84f551c64979dc0b7de7210502c18
arvind412 <40143544+arvind412@users.noreply.github.com>
2025-10-03 16:56:29 +0530

    Create main.tf

---
+++ terraform/aws/modules/networking/main.tf
@@ -0,0 +1,22 @@
+data "aws_availability_zones" "available" {}
+resource "aws_vpc" "main" {
+cidr_block           = var.vpc_cidr_block
+enable_dns_support   = true
+enable_dns_hostnames = true
+tags = merge(var.tags, { Name = "vpc-main" })
+}
+resource "aws_subnet" "public" {
+count                   = length(var.public_subnet_cidrs)
+vpc_id                  = aws_vpc.main.id
+cidr_block              = var.public_subnet_cidrs[count.index]
+map_public_ip_on_launch = true
+availability_zone       = element(data.aws_availability_zones.available.names, count.index)
+tags = merge(var.tags, { Name = "public-${count.index}" })
+}
+resource "aws_subnet" "private" {
+count             = length(var.private_subnet_cidrs)
+vpc_id            = aws_vpc.main.id
+cidr_block        = var.private_subnet_cidrs[count.index]
+availability_zone = element(data.aws_availability_zones.available.names, count.index)
+tags = merge(var.tags, { Name = "private-${count.index}" })
+}
